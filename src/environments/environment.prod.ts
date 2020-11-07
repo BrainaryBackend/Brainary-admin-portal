65b03718fd50940b15e992e674f50b29e395da0e
Jayarmananan1994 <dhoniramana@gmail.com>
2020-11-07 13:32:07 +0530

    Add the production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAF4OSDx2wm8MCcrbtUUECIk4fWNg9uh2A",
+    authDomain: "beliefhack-brainery-app.firebaseapp.com",
+    databaseURL: "https://beliefhack-brainery-app.firebaseio.com",
+    projectId: "beliefhack-brainery-app",
+    storageBucket: "beliefhack-brainery-app.appspot.com",
+    messagingSenderId: "444794876147",
+    appId: "1:444794876147:web:dd886c25875bfaf9180786",
+    measurementId: "G-PFGQTKGJDN"
+  }
 };
